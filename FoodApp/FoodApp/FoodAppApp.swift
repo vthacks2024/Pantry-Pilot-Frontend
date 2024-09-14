981757359e279db8de8f0d9ff543671d6e6df0ee
ericjung04 <ericjung1705@gmail.com>
2024-09-14 13:06:26 -0400

    Initial Commit

---
+++ FoodApp/FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by Eric Jung on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
